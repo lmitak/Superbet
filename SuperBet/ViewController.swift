e1f108ad3a19bd4393e042efdbadd5308e501e53
Luka Mitak <lukamitak@darios-macbook-pro.local>
2017-03-30 11:45:31 +0200

    Initial Commit

---
+++ SuperBet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperBet
+//
+//  Created by Luka Mitak on 30/03/17.
+//  Copyright © 2017 BetInvent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
